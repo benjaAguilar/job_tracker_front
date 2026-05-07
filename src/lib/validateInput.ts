40a80c740b2e7d070e10a4d7ec53694ad2357bf2
Benja Aguilar <benjaaguilar02@gmail.com>
2026-05-07 15:44:02 -0300

    feat: add validate input builder
    this builder focuses on create diferent validation behaviours for
    different input situations!
    thanks to Refactoring.guru and gentelman programming! i finally applied
    a pattern on a project!

---
+++ src/lib/validateInput.ts
@@ -0,0 +1,79 @@
+type Fn = (input: string, fieldName: string) => string | null;
+
+export class InputValidator {
+  validations = new Map<string, Fn | undefined>();
+
+  constructor(builder: InputValidatorBuilder) {
+    this.validations.set("notEmpty", builder.notEmpty);
+    this.validations.set("length", builder.length);
+    this.validations.set("isEmail", builder.isEmail);
+    this.validations.set("isAplhaNumeric", builder.isAplhaNumeric);
+  }
+
+  validate(input: string, fieldName: string) {
+    const errMessages: Array<string> = [];
+    this.validations.forEach((val) => {
+      const res = val?.(input, fieldName);
+      res ? errMessages.push(res) : null;
+    });
+
+    return errMessages;
+  }
+}
+
+export class InputValidatorBuilder {
+  notEmpty?: Fn;
+  length?: Fn;
+  isEmail?: Fn;
+  isAplhaNumeric?: Fn;
+
+  setNotEmpty() {
+    this.notEmpty = (input: string, fieldName: string) => {
+      if (!input) {
+        return `${fieldName} is required`;
+      }
+      return null;
+    };
+    return this;
+  }
+
+  setLength(min: number, max: number) {
+    this.length = (input: string, fieldName: string) => {
+      if (input.length < min) {
+        return `${fieldName} length should be at least ${min} characters`;
+      }
+
+      if (input.length > max) {
+        return `${fieldName} length should be at most ${max} characters`;
+      }
+      return null;
+    };
+    return this;
+  }
+
+  setIsEmail() {
+    this.isEmail = (input: string, fieldName: string) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(input)) {
+        return `${fieldName} should be a valid email`;
+      }
+      return null;
+    };
+    return this;
+  }
+
+  setIsAlphaNumeric() {
+    this.isAplhaNumeric = (input: string, fieldName: string) => {
+      const alphaRegex = /^[a-zA-Z0-9]+$/;
+      if (!alphaRegex.test(input)) {
+        return `${fieldName} should only have alphanumeric characters`;
+      }
+      return null;
+    };
+    return this;
+  }
+
+  build() {
+    return new InputValidator(this);
+  }
+}
