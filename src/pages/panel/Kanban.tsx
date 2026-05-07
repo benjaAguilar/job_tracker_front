6fdfb43633af43a5a5874ba538a2bcb1805de557
Benja Aguilar <benjaaguilar02@gmail.com>
2026-05-07 09:41:12 -0300

    feat: add DragDropProvider, add Panel component

--- src/pages/panel/Kanban.tsx
+++ src/pages/panel/Kanban.tsx
@@ -1,17 +1,36 @@
-import { KanbanCard } from "@/components/ui/KanbanCard";
+import { JobCard } from "@/components/ui/JobCard";
+import { KanbanCols } from "@/components/ui/KanbanCols";
+import { DragDropProvider } from "@dnd-kit/react";
+import { move } from "@dnd-kit/helpers";
+import { useState } from "react";
+import { Panel } from "@/components/Panel";
 
 export function Kanban() {
+  const [itemsInCols, setItemsInCols] = useState({
+    Offer: ["a1"],
+    Applied: ["b2"],
+    Interview: ["c1", "c2"],
+    Rejected: [],
+  });
+
   return (
-    <section className="grid grid-cols-[280px_1fr] h-full ">
-      <div className="border-r-2 shadow bg-gray-200 h-full ">
-        <h1>Panel</h1>
-      </div>
-      <div className="grid grid-cols-4 gap-3 p-7">
-        <KanbanCard title="Offer" />
-        <KanbanCard title="Applied" />
-        <KanbanCard title="Interview" />
-        <KanbanCard title="Rejected" />
-      </div>
+    <section className="grid grid-cols-[280px_1fr] h-full">
+      <Panel />
+      <DragDropProvider
+        onDragOver={(event) => {
+          setItemsInCols((items) => move(items, event));
+        }}
+      >
+        <div className="grid grid-cols-4 gap-3 p-7">
+          {Object.entries(itemsInCols).map(([col, items]) => (
+            <KanbanCols key={col} id={col} title={col}>
+              {items.map((item, index) => (
+                <JobCard id={item} key={item} index={index} column={col} />
+              ))}
+            </KanbanCols>
+          ))}
+        </div>
+      </DragDropProvider>
     </section>
   );
 }
