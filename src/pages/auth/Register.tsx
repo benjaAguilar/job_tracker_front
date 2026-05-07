482341f98c462f1c3a593bb4ab286858daeb6bac
Benja Aguilar <benjaaguilar02@gmail.com>
2026-05-07 15:48:16 -0300

    feat: add use state to handle form submit and inputWithVal component

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -8,36 +8,61 @@ import {
   FieldLegend,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { InputWithValidation } from "@/components/ui/InputWithValidation";
+import { InputValidatorBuilder } from "@/lib/validateInput";
+import { useState } from "react";
 
 export function Register() {
+  const [validation, setValidations] = useState<string[]>([]);
+
   return (
     <section className="flex items-center justify-center h-full p-7">
       <Card className="w-2xl p-3">
-        <form>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (validation.length >= 1) return alert("complete the fields");
+          }}
+        >
           <FieldGroup className="flex flex-col items-center justify-center gap-0 mb-7">
-            <FieldLegend className="">Register on Tracki'n</FieldLegend>
+            <FieldLegend className="font-heading">
+              Register on Tracki'n
+            </FieldLegend>
             <FieldDescription>
               Create a user to start tracking your job applications like a pro
               😎
             </FieldDescription>
           </FieldGroup>
           <FieldGroup className="grid grid-cols-1 md:grid-cols-2">
-            <Field>
-              <FieldLabel htmlFor="username">Username</FieldLabel>
-              <Input
+            <Field className="gap-0">
+              <FieldLabel htmlFor="username" className="m-0">
+                Username
+              </FieldLabel>
+              <InputWithValidation
                 type="text"
                 id="username"
+                name="username"
                 placeholder="pickle_rick"
-                required
+                validations={new InputValidatorBuilder()
+                  .setNotEmpty()
+                  .setLength(4, 30)
+                  .setIsAlphaNumeric()
+                  .build()}
+                setValidationMessages={setValidations}
               />
             </Field>
-            <Field>
+            <Field className="gap-0">
               <FieldLabel htmlFor="email">Email</FieldLabel>
-              <Input
+              <InputWithValidation
                 type="email"
                 id="email"
+                name="email"
                 placeholder="pickle@gmail.com"
-                required
+                validations={new InputValidatorBuilder()
+                  .setNotEmpty()
+                  .setIsEmail()
+                  .build()}
+                setValidationMessages={setValidations}
               />
             </Field>
             <Field>
