a49c76e8f6debc5a878509fc4d260cad9c8d4ddf
Benja Aguilar <benjaaguilar02@gmail.com>
2026-05-08 12:51:51 -0300

    feat: add validation input on login

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -8,8 +8,13 @@ import {
   FieldLegend,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { InputWithValidation } from "@/components/ui/InputWithValidation";
+import { InputValidatorBuilder } from "@/lib/validateInput";
+import { useState } from "react";
 
 export function Login() {
+  const [validation, setValidations] = useState<string[]>([]);
+
   return (
     <section className="flex items-center justify-center h-full p-7">
       <Card className="w-2xl p-3">
@@ -19,20 +24,31 @@ export function Login() {
             <FieldDescription>Login to your account</FieldDescription>
           </FieldGroup>
           <FieldGroup className="grid grid-cols-1">
-            <Field>
+            <Field className="gap-0">
               <FieldLabel htmlFor="usernameOrEmail">
                 Username or Email
               </FieldLabel>
-              <Input
+              <InputWithValidation
                 type="text"
                 id="usernameOrEmail"
-                placeholder="pickle_rick"
-                required
+                name="usernameOrEmail"
+                placeholder="picklerick33"
+                validations={new InputValidatorBuilder()
+                  .setNotEmpty()
+                  .setLength(4, 100)
+                  .build()}
+                setValidationMessages={setValidations}
               />
             </Field>
-            <Field>
+            <Field className="gap-0">
               <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Input type="password" id="password" required />
+              <InputWithValidation
+                type="password"
+                id="password"
+                name="password"
+                validations={new InputValidatorBuilder().setNotEmpty().build()}
+                setValidationMessages={setValidations}
+              />
             </Field>
             <Button type="submit">Login</Button>
           </FieldGroup>
